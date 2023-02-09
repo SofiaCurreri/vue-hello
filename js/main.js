c3228736384cd7683104212769233ef0d93b2424
SofiaCurreri <sofiacurreri@gmail.com>
2023-02-09 16:23:16 +0100

    esercizio completato

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Stampare a schermo un messaggio all'interno di un h1, utilizzando i data.
+// Bonus:
+// Aggiungere alla pagina un'immagine, presa anch'essa da un data.
+
+console.log("Vue ok", Vue);
+const app = Vue.createApp({
+    data() {
+        return {
+            message: "Ciao! Sto imparando ad usare Vue.js"
+        }
+    }
+});
+app.mount("#root");
